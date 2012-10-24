8c75d454f3bcdaede50035252cf3200dc5578554
Star <jn615@126.com>
2012-10-24 20:57:02 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Didiaodian::Application.config.secret_token = 'f58b604c07fbffaf8c5e34f1c72b12caeb8cbc110b0ddb8847ea8714d0f38a1e392c3949f9dd202462a1c3a31df016e6a447566a7ae6e7b27acfed5487ac0114'
